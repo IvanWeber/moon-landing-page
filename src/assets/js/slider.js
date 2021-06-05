d3c62470dbee929cbde755f70dd56ff25396bfa8
IvanWeber <ivanbdn@yandex.ru>
2021-06-06 00:06:41 +0300

    adapt slider

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -30,6 +30,34 @@ $(document).ready(function() {
     touchThreshold: 5,
     touchMove: false,
     waitForAnimate: true,
-    // centerMode: true
+    centerMode: false,
+    variableWidth: false,
+    rows: 1,
+    // slidesPerRoll: 1,
+    vertical: false,
+    verticalSwiping: false,
+    responsive: [
+      {
+        breakpoint: 1250,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   })
 })
